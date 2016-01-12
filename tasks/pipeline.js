ff9be0f8c06c72f864652a660bf4b396655e5b59
gcarey <gcarey@vaynermedia.com>
2016-01-12 10:45:48 -0500

    Remove sails.io script loading from pipeline

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -25,14 +25,14 @@ var cssFilesToInject = [
 var jsFilesToInject = [
 
   // Load sails.io before everything else
-  'js/dependencies/sails.io.js',
+  // 'js/dependencies/sails.io.js',
 
-  // Dependencies like jQuery, or Angular are brought in here
-  'js/dependencies/**/*.js',
+  // // Dependencies like jQuery, or Angular are brought in here
+  // 'js/dependencies/**/*.js',
 
-  // All of the rest of your client-side js files
-  // will be injected here in no particular order.
-  'js/**/*.js',
+  // // All of the rest of your client-side js files
+  // // will be injected here in no particular order.
+  // 'js/**/*.js',
 
   // Use the "exclude" operator to ignore files
   // '!js/ignore/these/files/*.js'
