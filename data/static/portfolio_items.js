c20bf0085b4caee6339372c7acd8450c09827b0b
gcarey <gcarey@vaynermedia.com>
2016-01-23 18:54:40 -0500

    Rework image overlays

--- data/static/portfolio_items.js
+++ data/static/portfolio_items.js
@@ -1,85 +1,4 @@
 module.exports = [{
-    "title": "Geolocation game admin",
-    "agency": "G2",
-    "agency_url": null,
-    "slug": "geolocation-game-admin",
-    "description": "I was on a team that built a first-person-shooter mobile game for Android and iOS. I helped to build the back-end administrative tool (ASP.NET MVC, C#) that allowed select users to log-in and create live, geolocation-based tournaments.  We used the mobile data stack Parse to store the tournament and user data.  I employed responsive design techniques so that the admin tool and leaderboard would be usable across various devices.",
-    "tools": [
-        "ASP.NET MVC",
-        "C#",
-        "CSS",
-        "HTML",
-        "Parse"
-    ],
-    "work_types": [
-        "Front-end",
-        "Back-end"
-    ],
-    "year": 2012,
-    "month": 11,
-    "thumb_url": "geo_game_thumb.jpg",
-    "image_urls": [
-        "geo_game_leaderboard.jpg",
-        "geo_game_admin.jpg"
-    ]
-},{
-    "title": "Slideover photo widget",
-    "client": "Elizabeth Arden New York",
-    "year": 2013,
-    "slug": "slideover-photo-widget",
-    "description": "I built a modal dual-layer photo slideover widget in order to help promote an anti-aging cosmetics product.  The widget features a draggable handle that reveals an image overlay as it is pulled left.  I wrote the JQuery module to be flexible, with multiple parameters so that it can be re-purposed later for other uses.",
-    "thumb_url": "slideover_thumb.jpg",
-    "image_urls": [
-        "slideover.jpg"
-    ],
-    "work_types": [
-        "Front-end"
-    ],
-    "tools": [
-        "JQuery",
-        "CSS"
-    ]
-},{
-    "title": "Fundraiser web app",
-    "agency": "TMP Worldwide",
-    "slug": "fundraiser-web-app",
-    "description": "I volunteered to design and build a web app for an internal initiative to raise money for the JDRF. The site is responsive, using a PHP/Mysql backend to store pledge data. I built the pledge thermometer widget entirely from CSS and custom data binding.",
-    "tools": [
-        "CSS",
-        "HTML5",
-        "Javascript",
-        "PHP",
-        "MySQL"
-    ],
-    "work_types": [
-        "Front-end",
-        "Back-end"
-    ],
-    "year": 2014,
-    "thumb_url": "fundraiser_thumb.png",
-    "image_urls": [
-        "fundraiser.png"
-    ]
-},{
-    "title": "Job candidate recruitment",
-    "slug": "job-candidate-recruitment",
-    "agency": "TMP Worldwide",
-    "agency_url": "http://www.tmp.com/",
-    "description": "I provided many clients with front-end code for TMP's job search recruitment web product. Work was based on a boilerplate and tailored to meet the needs of the client.",
-    "tools": [
-        "CSS",
-        "HTML5",
-        "Javascript"
-    ],
-    "work_types": [
-        "Front-end"
-    ],
-    "year": 2014,
-    "thumb_url": "job_candidate_thumb.jpg",
-    "image_urls": [
-        "job_candidate.jpg"
-    ]
-},{
     "title": "Admin tools",
     "agency": "VaynerMedia",
     "slug": "internal-company-admin",
@@ -103,18 +22,143 @@ module.exports = [{
         "Knockout",
         "Browserify"
     ]
-}, {
-    "title": "Drum machine",
-    "slug": "internal-company-admin",
+},{
+    "title": "Job candidate recruitment",
+    "slug": "job-candidate-recruitment",
+    "agency": "TMP Worldwide",
+    "agency_url": "http://www.tmp.com/",
+    "description": "I provided many clients with front-end code for TMP's job search recruitment web product. Product implementations were based on a boilerplate and tailored to meet the needs of the client.",
+    "tools": [
+        "CSS",
+        "HTML5",
+        "Javascript"
+    ],
+    "work_types": [
+        "Front-end"
+    ],
+    "year": 2014,
+    "thumb_url": "job_candidate_thumb.jpg",
+    "image_urls": [
+        "job_candidate.jpg"
+    ]
+},{
+    "title": "Fundraiser web app",
+    "agency": "TMP Worldwide",
+    "slug": "fundraiser-web-app",
+    "description": "I volunteered to design and build a web app for an internal company initiative to raise money for the JDRF. The site is responsive, using a PHP/Mysql backend to store pledge data. The pledge thermometer widget is built entirely from CSS and custom data binding.",
+    "tools": [
+        "CSS",
+        "HTML5",
+        "Javascript",
+        "PHP",
+        "MySQL"
+    ],
+    "work_types": [
+        "Front-end",
+        "Back-end"
+    ],
+    "year": 2014,
+    "thumb_url": "fundraiser_thumb.png",
+    "image_urls": [
+        "fundraiser.jpg"
+    ]
+},{
+    "title": "Slideover photo widget",
+    "client": "Elizabeth Arden New York",
+    "year": 2013,
+    "slug": "slideover-photo-widget",
+    "description": "I built a modal dual-layer photo slideover widget in order to help promote an anti-aging cosmetics product.  The widget features a draggable handle that reveals an image overlay as it is pulled left.  I wrote the JQuery module to be flexible, with multiple parameters so that it can be re-purposed later for other uses.",
+    "thumb_url": "slideover_thumb.jpg",
+    "image_urls": [
+        "slideover.jpg"
+    ],
+    "work_types": [
+        "Front-end"
+    ],
+    "tools": [
+        "JQuery",
+        "CSS"
+    ]
+},{
+    "title": "Geolocation game admin",
+    "agency": "G2",
+    "agency_url": null,
+    "year": 2012,
+    "slug": "geolocation-game-admin",
+    "description": "I was on a team that built a first-person-shooter mobile game for Android and iOS. I helped to build the back-end administrative tool (ASP.NET MVC, C#) that allowed select users to log-in and create live, geolocation-based tournaments.  We used the mobile data stack Parse to store the tournament and user data.  I employed responsive design techniques so that the admin tool and leaderboard would be usable across various devices.",
+    "tools": [
+        "ASP.NET MVC",
+        "C#",
+        "CSS",
+        "HTML",
+        "Parse"
+    ],
+    "work_types": [
+        "Front-end",
+        "Back-end"
+    ],
+    "month": 11,
+    "thumb_url": "geo_game_thumb.jpg",
+    "image_urls": [
+        "geo_game_leaderboard.jpg",
+        "geo_game_admin.jpg"
+    ]
+},{
+    "title": "Non profit CMS",
+    "agency": "G2",
+    "slug": "non-profit-cms",
+    "description": "This non-profit client required a robust, intuitive CMS that would allow them to easily make content updates. After planning the site architecture and taxonomy, I built this CMS in the PHP-based Wordpress framework. I was provided .psds from which I created custom CSS stylesheets. One of the more complex features on the site is the  Clinical Trials search, which allows the user to filter multiple search results in real-time.  It required organizing a custom content type with complex taxonomy reflecting the multiple optional filters.",
+    "tools": [
+        "PHP",
+        "Wordpress",
+        "JQuery",
+        "HTML5",
+        "CSS"
+    ],
+    "work_types": [
+        "Front-end",
+        "Back-end"
+    ],
+    "year": 2012,
+    "thumb_url": "nonprofit_thumb.jpg",
+    "image_urls": [
+        "nonprofit_home.jpg"
+    ]
+},{
+    "title": "Comic Book Company",
+    "slug": "comic-book-company",
+    "description": "I developed and designed this website for an independent comic book company.  The client required the ability to update news items, videos, store products, slideshow items, and new comic book releases.  Features integration with Graphic.ly, an online comic-book browser, an online store, a jQuery slideshow, and a Twitter feed.",
+    "tools": [
+        "Drupal",
+        "PHP",
+        "JQuery",
+        "Javascript",
+        "HTML",
+        "CSS"
+    ],
+    "work_types": [
+        "Front-end",
+        "Back-end"
+    ],
     "year": 2011,
-    "description": "As a personal project, I built and programmed a simple arduino drumb machine",
+    "thumb_url": "idcomics_thumb.png",
+    "image_urls": [
+        "idcomics_home.png"
+    ]
+},{
+    "title": "Drum machine",
+    "slug": "drum-machine",
+    "year": 2011,
+    "description": "As a personal project, I built and programmed a toy arduino drum machine",
     "thumb_url": "drummachine_thumb.png",
     "image_urls": [
     ],
     "work_types": [
-        "Back-end"
+        "Back-end",
+        "Hardware"
     ],
     "tools": [
         "Arduino"
-    ]
+    ],
+    "youtube_url": "https://www.youtube.com/embed/CxEV4SAEYjs"
 }];
